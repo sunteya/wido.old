ac0ff0d2d593cc8403f4d87fb6eb3b49d705c867
Sunteya <Sunteya@gmail.com>
2012-05-11 12:15:21 +0800

    integrate octopress

--- plugins/000-init.rb
+++ plugins/000-init.rb
@@ -1,1 +1,12 @@
 require "pry-nav"
+
+octopress = File.expand_path("../../octopress", __FILE__)
+Dir.chdir(octopress) do
+  Dir[File.join("plugins", "**/*.rb")].each do |f|
+    file = File.absolute_path(f)
+    puts file
+    require file
+  end
+end
+
+
