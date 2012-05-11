bbca08eb404b308f2a1c8d459e6c01b65c07623f
Sunteya <Sunteya@gmail.com>
2012-05-11 23:41:28 +0800

    plugins/octopress

--- plugins/000-init.rb
+++ plugins/000-init.rb
@@ -2,9 +2,3 @@ require "pry-nav"
 require "active_support/all"
 require "coderay_bash"
 
-octopress = File.expand_path("../../octopress", __FILE__)
-Dir.chdir(octopress) do
-  Dir[File.join("plugins", "**/*.rb")].each do |f|
-    require File.absolute_path(f)
-  end
-end
